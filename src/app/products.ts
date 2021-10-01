4953dcdcf2da6655711a34817928c6dcbf694e82
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-10-01 20:58:13 -0300

    atualizacao

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,24 +8,26 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Processador AMD Ryzen 5 5600x',
+    price: 1800,
+    description:
+      'Processador AMD Ryzen 5 5600X Jogue com os melhores 6 núcleos incríveis para quem quer apenas jogar. Obtenha o desempenho de jogos de alta velocidade do melhor processador de desktop do mundo',
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Placa-Mãe Asus ROG Strix X570',
+    price: 3100,
+    description:
+      'A placa-mãe ROG Strix X570-E Gaming oferece a experiência ROG por excelência com uma combinação atraente de recursos e estilo inegável que fornecem a base ideal para a criação de um poderoso sistema de jogos ATX.',
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
-  }
+    name: 'Water Cooler NZXT Kraken Z73',
+    price: 1400,
+    description:
+      'A Série Kraken X atualizada é essencial para qualquer construção, proporcionando melhor resfriamento, efeitos visuais impressionantes e instalação intuitiva. O novo Kraken X oferece uma experiência incrível em refrigeração líquida RGB.',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
